a9259a4deaeb8ae00225591359db308d78cc6ce9
Matt Mills <mmills@2bn.net>
2023-07-31 22:55:39 -0600

    redo tag names and add some values

--- nbt-test.py
+++ nbt-test.py
@@ -1,5 +1,7 @@
 import gzip
 import python_nbt
+import struct
+
 depth = 0
 
 def nbt_read_string(fh):
@@ -14,7 +16,10 @@ def nbt_read(fh):
     global depth
     #it is assumed that the next byte is a tagType
 
-    tag_type = ord(fh.read(1))
+    byte_in = fh.read(1)
+    if len(byte_in) == 0:
+        return False
+    tag_type = ord(byte_in)
     if tag_type != 0:
         name = nbt_read_string(fh)
     else:
@@ -27,35 +32,39 @@ def nbt_read(fh):
         #TAG_End
         tag = 'End'
     elif tag_type == 1:
-        tag = 'Integer'
-        value = int.from_bytes(fh.read(4), byteorder='little')
+        tag = 'int32_t'
+        value = int.from_bytes(fh.read(4), byteorder='little', signed=True)
     elif tag_type == 2:
-        tag = 'Short'
-        fh.read(4)
+        tag = 'float'
+        byte_string = fh.read(4)
+    
+        value = struct.unpack('<f', byte_string)[0]
     elif tag_type == 3:
-        tag = 'String?'
+        tag = 'String'
         length = int.from_bytes(fh.read(2), byteorder='little')
         string = fh.read(length)
         
         value = '(len: %s) "%s"'  % (length, string)
         
     elif tag_type == 4:
-        tag = 'Long'
-        fh.read(8)
+        tag = '5 byte unk'
+        fh.read(5)
     elif tag_type == 5:
-        tag = 'Float'
-        fh.read(2)
+        tag = 'short?'
+        value = int.from_bytes(fh.read(2), byteorder='little')
     elif tag_type == 6:
-        tag = 'Double'
-        fh.read(8)
+        tag = 'Double?'
+        byte_string = fh.read(8)
+        value = struct.unpack('<d', byte_string)[0]
     elif tag_type == 7:
-        tag = 'Byte_Array'
+        tag = 'Byte_Array?'
         length = int.from_bytes(fh.read(4), byteorder='little') 
         value = length
         fh.read(length)
     
     elif tag_type == 8:
-        tag = 'String'
+        tag = 'byte unk1'
+        value = int.from_bytes(fh.read(1), byteorder='little')
     elif tag_type == 9:
         tag = 'List'
         sub_tag_id = int.from_bytes(fh.read(4), byteorder='little')
@@ -65,29 +74,37 @@ def nbt_read(fh):
         #TAG_Compound
         tag = "Compound"
     elif tag_type == 11: # int array?
+        tag = 'int_array?'
         length = int.from_bytes(fh.read(4), byteorder='little')
         value = length
         fh.read(length*4)
     elif tag_type == 227:
+        tag = '6 byte something'
         fh.read(6)
     elif tag_type == 228:
+        tag = '16 byte variable length array'
         length = int.from_bytes(fh.read(4), byteorder='little')
         value = length
         fh.read(length*16)
         
     elif tag_type == 229:
+        tag = '16  byte unk'
         fh.read(16)
     elif tag_type == 230:
-        fh.read(1)
+        tag = 'byte unk2' #bool?
+        value = int.from_bytes(fh.read(1), byteorder='little')
     elif tag_type == 243:
+        tag = 'int array 2?'
         length = int.from_bytes(fh.read(4), byteorder='little')
         value = length
         fh.read(length*4)
     elif tag_type == 244:
+        tag = '8 byte array?'
         length = int.from_bytes(fh.read(4), byteorder='little')
         value = length
         fh.read(length*8)
     elif tag_type == 247:
+        tag = '2 byte array?'
         length = int.from_bytes(fh.read(4), byteorder='little')
         value = length
         fh.read(length*2)
@@ -97,6 +114,9 @@ def nbt_read(fh):
         depth += 1
     if tag_type == 0:
         depth -= 1
+
+    return True
+
 with open('test1.cw4', mode='rb') as fh:
     expected_file_size = int.from_bytes(fh.read(4), byteorder="little")
 
@@ -105,8 +125,9 @@ with open('test1.cw4', mode='rb') as fh:
     #measured_file_size = len(contents)
     #gzfh.seek(4)
 
-    while(1):
-        nbt_read(gzfh)
+    ret = True
+    while(ret):
+        ret = nbt_read(gzfh)
         
 
 
